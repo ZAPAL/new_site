b22d91bc6cbf8947c0ede3b1aa58efea18d9e466
ZAPAL <92725Goga>
2019-09-30 09:29:25 +0300

    first commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
 echo "TEST SITE!!";
 ?> 
-
+Hello world
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
